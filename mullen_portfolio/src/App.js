281fa442d8de50ae639472077dad066f3e8ba8cc
armullen <amber.r.mullen@gmail.com>
2023-07-25 22:10:06 -0500

    working a bug with bootstrap

--- mullen_portfolio/src/App.js
+++ mullen_portfolio/src/App.js
@@ -1,7 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 // ............components.................
 import Header from "./components/Header";
--- mullen_portfolio/src/pages/Projects.js
+++ mullen_portfolio/src/pages/Projects.js
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Projects () {
     return (
         <div className="projectsPage">
-        <h1>Check out some of my code!</h1>
-        <Link to='https://armullen.github.io/ProjectOne---SimonGame/'>Simon Game</Link>
-        <img className='simon' src='/assets/simon.png' alt='simon'></img>
-        <h1>Hockey Collector</h1>
-        <h1>ProStart Sports</h1>
+            <Container>
+                <Row>
+                <Col>
+                     <h1>Check out some of my code!</h1>
+                    <Link to='https://armullen.github.io/ProjectOne---SimonGame/'>Simon Game</Link>
+                    <h5>A browser game of memory using JavaScript and CSS.  It was my first project in my Software Engineering Immersive.</h5>
+                    <img className='simon' src='/assets/simon.png' alt='simon'></img>
+                </Col>
+                <Col>
+                    <Link to='https://github.com/armullen/HockeyCollector_MiniProject'>Hockey Collector</Link>
+                    <h5>A favorite hockey player collector app using Django and Python.</h5>
+                    <h1>ProStart Sports</h1>
+                    <h5>A site I am working on for a local sports training facility using React, Django, and PostgreSQL.</h5>
+                </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
